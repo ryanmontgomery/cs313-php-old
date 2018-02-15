357fe85d94bf20e09108da36b1f7d795ddf5cae5
Ryan Montgomery <mon16032@byui.edu>
2018-02-14 23:43:54 -0700

    Changed db settings for online

--- web/assignments/project1/model/database.php
+++ web/assignments/project1/model/database.php
@@ -1,13 +1,13 @@
 <?php
     //localhost
-    $dsn = 'pgsql:host=localhost;dbname=mydb';
-    $username = 'postgres';
-    $password = 'monkeytoo2';
-    
-    //Remote host settings
-    //$dsn = 'pgsql:host=localhost;dbname=library';
+    //$dsn = 'pgsql:host=localhost;dbname=mydb';
     //$username = 'postgres';
     //$password = 'monkeytoo2';
+    
+    //Remote host settings
+    $dsn = 'pgsql:host=localhost;dbname=hobby-dev';
+    $username = 'postgres';
+    $password = 'monkeytoo2';
 
     try {
         $db = new PDO('pgsql:host=localhost;dbname=mydb;user=postgres;password=monkeytoo2');
